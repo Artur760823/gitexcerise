53d706b0b48aa0fab52a124c72ee2b4254399ec9
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2020-12-17 10:59:04 +0100

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println(" And this is another change...yeah this is super cool program");
         System.out.println(" And this is another change...my program is growing");
         System.out.println("Another commit...yey...right");
+        System.out.println("Test");
     }
 }
