0bea0b8dbe0fb9ff2ac3323e62eae312e9b30c68
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2020-12-17 11:33:29 +0100

    Added person class

---
+++ src/com/company/Person.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Person {
+    private String name;
+    private String city;
+    private int age;
+
+    public Person(String name, String city, int age) {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+}
