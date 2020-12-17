6ddb7884ef5e5a7852b3f248a406acde26346ddd
CORPO\AZajaczkowski <artur.zajaczkowski@t-mobile.pl>
2020-12-17 10:47:48 +0100

    another commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I'm master of GIT");
+        System.out.println(" And this is another change...yeah this is super cool program");
     }
 }
